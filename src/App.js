9f5a6227f6e3dc5c951b648a57ac9139f566e9f8
AMAN BARACH <129427969+barachaman04@users.noreply.github.com>
2023-08-26 13:44:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+import './App.css';
+import { CalculatorPage } from "./page/Calculatorpage";
+
+function App() {
+  return (
+    <CalculatorPage/>
+  );
+}
+
+export default App;
